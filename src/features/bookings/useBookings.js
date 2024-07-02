b290a424b0be8911c7e6ab12dccc6f7831da5c0e
Ahmed5Atiya <129235416+Ahmed5Atiya@users.noreply.github.com>
2024-07-02 06:25:19 +0300

    i use the prefetchQuery this is mean if i in the page 2 i make the page 3 in the cashe to the user cont show the loader spanner this is make the next page in the cashe

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 import SortedBy from "../../ui/SortedBy";
 import Pagination from "../../ui/Pagination";
+import { PAGE_SIZE } from "../../utils/Constant";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   const filterValue = searchParams.get("status");
   const filter =
@@ -25,6 +27,20 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page], //this mean when the filter change the reactQuery will re fetch the data again
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
   return { isLoading, bookings, error, count };
 }
 
